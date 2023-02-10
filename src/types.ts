be882dd0eeba0d1f592d381bd433fe61a4cb04f7
Eric Crosson <eric.s.crosson@utexas.edu>
2023-02-09 18:47:49 -0600

    chore: use jsdoc syntax
    to support LSP

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,16 @@
-// The following formats are valid:
-// - v1
-// - v1.2
-// - v1.2.3
+/**
+ * The following formats are valid:
+ * - v1
+ * - v1.2
+ * - v1.2.3
+ */
 export type SemanticVersion =
   | ExactSemanticVersion
   | (string & { readonly __tag: unique symbol });
 
-// The following format is valid: v1.2.3
+/**
+ * The following format is valid: v1.2.3
+ */
 export type ExactSemanticVersion = string & { readonly __tag: unique symbol };
 
 export function isExactSemanticVersion(
